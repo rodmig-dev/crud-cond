6e20fd1cd77eb4610f96a8504fdc77561df57d61
rodmigdev <rodmig2013@gmail.com>
2025-10-02 22:20:30 -0300

    Começando a codar

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import '/.App.css';
+
+//Importa os componentes de seus respectivos arquivos
+import Login from './Login/Login';
+import Menu from './Menu/Menu';
+import Welcome from'./Welcome/Welcome';
+import ContactList from './ContactForm/ContactList';
+import ContactForm from './ContactForm/ContactForm';
+
+
+function App(){
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [activeScreen, setActiveScreen] = useState('welcome');
+  const [contacts, setContacts] = useState([
+    {id: 1, name: 'João Silva', phone: '11987654321'},
+    {id: 2, name: 'Maria Silva', phone: '11123456789'}]);
+
+  const [contactToEdit, setContactToEdit] = useState(null);
+  
+
+    //Função de Login
+    const handleLogin = (userName, password) => {
+      if (userName === 'admin' && password === '123'){
+        setIsLoggedIn(true)
+      }else{
+        alert('Usuário ou senha inválida')
+      }
+    };
+
+    const handleSaveContact = (contact) => {}
+    const handleDeleteContact = (id) => {}
+    const startEdit = (contact) => {}
+    const showCreateForm = () => {}
+    const handleNavigate = (screen) => {}
+
+
+    if(!isLoggedIn){
+      return <Login onLogin = {handleLogin}/>
+    }
+
+    return(
+      <div>
+        <Menu onNavigate = {handleNavigate} onCreate={showCreateForm}/>
+        <main className="content">
+          {activeScreen === 'welcome' && <welcome/>}
+          {activeScreen === 'list' && <ContactList contacts={contacts}
+                             onEdit={startEdit} 
+                             onDelete={handleDeleteContact}/>}
+          {activeScreen === 'form' && <ContactForm
+                              contactToEdit={contactToEdit}
+                              onSave={handleSaveContact}/>}
+        </main>
+      </div>
+    )
+
+}export default App;
---
+++ src/Login/Login.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import './Login.css'
+
+function Login({onLogin}){
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handelSubmit = (e) => {
+        e.preventDefault()
+        onLogin(username, password);
+    }
+    return(
+        <div>
+            <form onSubmit={handelSubmit} >
+                <h2>Agenda de Contatos</h2>
+                <p>Faça login para continuar</p>
+                <input
+                    type="text"
+                    placeholder="Usuário (admin)"
+                    value={username}
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="(123)"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </form>
+        </div>
+    )
+
+}export default Login;
